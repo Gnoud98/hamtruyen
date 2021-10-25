bb36226192631820b27fb2c165373d71088bda9b
Gnoud98 <hiepsilun98@gmail.com>
2021-10-25 18:49:01 +0700

    update file

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
   window.onload = function () {
     $(document).ready(function () {
       showSearchMb();
+      menuMb();
     });
   };
 })(jQuery);
@@ -24,5 +25,34 @@ function showSearchMb() {
     $(".overlay").removeClass("overlay-active");
     $("body").removeClass("hidden");
   });
+}
 
+function menuMb() {
+  $(".header__bars ").click(function () {
+    $(".menu-mobile").addClass("show");
+    $(".overlay").addClass("overlay-active");
+    $("body").addClass("hidden");
+  });
+  $(".overlay").click(function () {
+    $(".menu-mobile").removeClass("show");
+    $(".overlay").removeClass("overlay-active");
+    $("body").removeClass("hidden");
+  });
+  $(".menu-mobile .close-btn").click(function () {
+    $(".menu-mobile").removeClass("show");
+    $(".overlay").removeClass("overlay-active");
+    $("body").removeClass("hidden");
+  });
+
+  $(".menu-mobile li.menu-has-mega>a").before("<span class='icon'></span>");
+  $(".menu-mobile li.menu-has-mega .icon").click(function () {
+    $(this).siblings(".mega-menu").slideToggle();
+    $(this).toggleClass("rotate");
+  });
+
+  $(".menu-mobile li.menu-item-has-children>a").before("<span class='icon'></span>");
+  $(".menu-item-has-children .icon").click(function () {
+    $(this).siblings("ul").slideToggle();
+    $(this).toggleClass("rotate");
+  });
 }
